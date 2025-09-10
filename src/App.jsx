86bb363d08fa798610795f0eaa28b3e98d14ca7d
Christobel <christobelnwachukwu@gmail.com>
2025-09-10 19:46:35 +0100

    creating my different routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import ShopCategory from "./Pages/ShopCategory"
 
 function App() {
 
   return (
   <div>
+    <Router>
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Shop />} />
+      <Route path='/men' element={<ShopCategory />}/>
+      <Route path='/women' element={<ShopCategory />}/>
+      <Route path='/kids' element={<ShopCategory />}/>
+    </Routes>
+
+    </Router>
+
   </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import "./Navbar.css"
 import logo from "../Assests/logo.png"
 import cart_icon from "../Assests/cart_icon.png"
+import { useState } from "react"
 const Navbar =()=> {
+
+const [menu, setMenu] =useState("shop")
+
     return (
         <div className="navbar"> 
         <div className="nav-logo">
@@ -9,10 +13,10 @@ const Navbar =()=> {
             <p>QuickMart</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop <hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu('shop')}}>Shop{menu ==='shop'? <hr/> : <></>}</li>
+            <li onClick={()=>{setMenu('men')}}>Men{menu ==='men'? <hr/> : <></>}</li>
+            <li onClick={()=>{setMenu('women')}}>Women{menu ==='women'? <hr/> : <></>}</li>
+            <li onClick={()=>{setMenu('kids')}}>Kids{menu ==='kids'? <hr/> : <></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
