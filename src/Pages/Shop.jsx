08471c80aa859346c70de82188c535eb3c816196
Christobel <christobelnwachukwu@gmail.com>
2025-09-12 00:40:24 +0100

    Finished creating the landing page for my e-commerce web-App

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,17 +1,19 @@
-import Hero from "../Components/Hero/Hero"
-import NewCollections from "../Components/NewCollections/NewCollections"
-import Offers from "../Components/Offers/Offers"
-import Popular from "../Components/Popular/Popular"
+import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Offers from "../Components/Offers/Offers";
+import Popular from "../Components/Popular/Popular";
 
-const Shop =()=> {
-    return (
-        <div>
-            <Hero /> 
-            <Popular /> 
-            <Offers />
-            <NewCollections />
-        </div>
-    )
-}
+const Shop = () => {
+  return (
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
